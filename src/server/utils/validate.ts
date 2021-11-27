b1fdc468bcc988216fbeee7e21b8e598c1c153e8
Freddy L <neo-ciber_94@hotmail.com>
2021-11-27 09:41:59 -0400

    Updated Validate

--- src/server/utils/validate.ts
+++ src/server/utils/validate.ts
@@ -9,36 +9,38 @@ export module Validate {
 
   export function isRequired(value: any, argName?: string): void {
     if (value === undefined || value === null) {
-      const message = argName
-        ? "value is required"
-        : `'${argName}' is required`;
+      const message =
+        argName == null ? "value is required" : `'${argName}' is required`;
       throw new ValidationError(message);
     }
   }
 
   export function isNonNullOrUndefined(value: any, argName?: string): void {
     if (value == null || value == undefined) {
-      const message = argName
-        ? "value cannot be null or undefined"
-        : `'${argName}' cannot be null or undefined`;
+      const message =
+        argName == null
+          ? "value cannot be null or undefined"
+          : `'${argName}' cannot be null or undefined`;
       throw new ValidationError(message);
     }
   }
 
   export function isNonEmptyString(value: any, argName?: string): void {
     if (typeof value !== "string" || value.length === 0) {
-      const message = argName
-        ? "value cannot be an empty string"
-        : `'${argName}' cannot be an empty string`;
+      const message =
+        argName == null
+          ? "value cannot be an empty string"
+          : `'${argName}' cannot be an empty string`;
       throw new ValidationError(message);
     }
   }
 
   export function isNonBlankString(value: any, argName?: string): void {
     if (typeof value !== "string" || value.trim().length === 0) {
-      const message = argName
-        ? "value cannot be a blank string"
-        : `'${argName}' cannot be a blank string`;
+      const message =
+        argName == null
+          ? "value cannot be a blank string"
+          : `'${argName}' cannot be a blank string`;
 
       throw new ValidationError(message);
     }
@@ -46,9 +48,10 @@ export module Validate {
 
   export function isString(value: any, argName?: string): value is string {
     if (typeof value !== "string") {
-      const message = argName
-        ? "value must be a string"
-        : `'${argName}' must be a string`;
+      const message =
+        argName == null
+          ? "value must be a string"
+          : `'${argName}' must be a string`;
 
       throw new ValidationError(message);
     }
@@ -58,9 +61,10 @@ export module Validate {
 
   export function isBoolean(value: any, argName?: string): value is boolean {
     if (typeof value !== "boolean") {
-      const message = argName
-        ? "value must be a boolean"
-        : `'${argName}' must be a boolean`;
+      const message =
+        argName == null
+          ? "value must be a boolean"
+          : `'${argName}' must be a boolean`;
       throw new ValidationError(message);
     }
 
@@ -69,9 +73,10 @@ export module Validate {
 
   export function isNumber(value: any, argName?: string): value is number {
     if (isNaN(Number(value))) {
-      const message = argName
-        ? "value must be a number"
-        : `'${argName}' must be a number`;
+      const message =
+        argName == null
+          ? "value must be a number"
+          : `'${argName}' must be a number`;
 
       throw new ValidationError(message);
     }
@@ -84,9 +89,10 @@ export module Validate {
     argName?: string
   ): value is number {
     if (isNaN(Number(value)) || value <= 0) {
-      const message = argName
-        ? "value must be a positive number"
-        : `'${argName}' must be a positive number`;
+      const message =
+        argName == null
+          ? "value must be a positive number"
+          : `'${argName}' must be a positive number`;
 
       throw new ValidationError(message);
     }
@@ -99,9 +105,10 @@ export module Validate {
     argName?: string
   ): value is number {
     if (isNaN(Number(value)) || value >= 0) {
-      const message = argName
-        ? "value must be a negative number"
-        : `'${argName}' must be a negative number`;
+      const message =
+        argName == null
+          ? "value must be a negative number"
+          : `'${argName}' must be a negative number`;
 
       throw new ValidationError(message);
     }
@@ -118,9 +125,10 @@ export module Validate {
     isRequired(value, argName);
 
     if (value < min || value > max) {
-      const message = argName
-        ? `value must be between ${min} and ${max}`
-        : `'${argName}' must be between ${min} and ${max}`;
+      const message =
+        argName == null
+          ? `value must be between ${min} and ${max}`
+          : `'${argName}' must be between ${min} and ${max}`;
 
       throw new ValidationError(message);
     }
@@ -132,9 +140,10 @@ export module Validate {
     argName?: string
   ): void {
     if (!values.includes(value)) {
-      const message = argName
-        ? `value must be one of [${values.join(", ")}]`
-        : `'${argName}' must be one of [${values.join(", ")}]`;
+      const message =
+        argName == null
+          ? `value must be one of [${values.join(", ")}]`
+          : `'${argName}' must be one of [${values.join(", ")}]`;
 
       throw new ValidationError(message);
     }
@@ -146,9 +155,10 @@ export module Validate {
     argName?: string
   ): void {
     if (values.includes(value)) {
-      const message = argName
-        ? `value must not be one of [${values.join(", ")}]`
-        : `'${argName}' must not be one of [${values.join(", ")}]`;
+      const message =
+        argName == null
+          ? `value must not be one of [${values.join(", ")}]`
+          : `'${argName}' must not be one of [${values.join(", ")}]`;
 
       throw new ValidationError(message);
     }
