b2e361a1ab05af68cee2b88f845c2951f12494b9
Freddy L <neo-ciber_94@hotmail.com>
2021-11-27 00:54:38 -0400

    Removed unused script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,10 @@ import "../styles/globals.css";
 import "../styles/TodoNote.css";
 import type { AppProps } from "next/app";
 import { Layout } from "src/components/Layout";
-import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
-      <Script src="sweetalert2/dist/sweetalert2.min.js"></Script>
       <Component {...pageProps} />
     </Layout>
   );
