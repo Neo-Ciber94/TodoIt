b781f6fe3ab0344f79a96fc6bbcead4b06e1f1ea
Freddy L <neo-ciber_94@hotmail.com>
2021-11-29 22:55:06 -0400

    Added Auth0 support

--- src/components/ButtonAppBar.tsx
+++ src/components/ButtonAppBar.tsx
@@ -1,15 +1,52 @@
-import { Box, Button, AppBar, Toolbar, styled } from "@mui/material";
+import {
+  Box,
+  Button,
+  AppBar,
+  Toolbar,
+  Typography,
+  styled,
+} from "@mui/material";
+import { useUser } from "@auth0/nextjs-auth0";
 
 const Offset = styled("div")(({ theme }) => theme.mixins.toolbar);
 
 export function ButtonAppBar() {
+  const { user, error, isLoading } = useUser();
+  const isLogin = user && !isLoading;
+
+  console.log(user);
+
+  // https://github.com/auth0/nextjs-auth0/issues/520#issuecomment-945756557
+  const goToLogin = () => window.location.assign("/api/auth/login");
+  const goToLogout = () => window.location.assign("/api/auth/logout");
+
+  if (error) {
+    console.error(error);
+  }
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="fixed" className="bg-black">
         <Toolbar>
-          <Button color="inherit" className="ml-auto">
-            Login
-          </Button>
+          <Box className="flex flex-row ml-auto">
+            {user && (
+              <div className="flex flex-col justify-center">
+                <Typography className="uppercase text-red-400 select-none">
+                  {`${user.name} - `}
+                </Typography>
+              </div>
+            )}
+            {!isLogin && (
+              <Button color="inherit" className="ml-auto" onClick={goToLogin}>
+                Login
+              </Button>
+            )}
+            {isLogin && (
+              <Button color="inherit" className="ml-auto" onClick={goToLogout}>
+                Logout
+              </Button>
+            )}
+          </Box>
         </Toolbar>
       </AppBar>
       <Offset />
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,15 @@ import "../styles/animations.css";
 import "../styles/TodoNote.css";
 import type { AppProps } from "next/app";
 import { Layout } from "src/components/Layout";
+import { UserProvider } from "@auth0/nextjs-auth0";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <UserProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </UserProvider>
   );
 }
 
