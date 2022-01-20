020da0c82199206ae31c6c6c397d8dc4f1e24aae
Freddy L <neo-ciber_94@hotmail.com>
2022-01-19 22:09:10 -0400

    Added mongodb middleware to tags

--- src/client/constants/index.ts
+++ src/client/constants/index.ts
@@ -1,7 +1,14 @@
 /**
  * The api URL.
  */
-export const API_URL =
-  process.env.NODE_ENV === "production"
-    ? process.env.NEXT_PUBLIC_VERCEL_URL
-    : process.env.NEXT_PUBLIC_API_URL;
+export const API_URL = getApiUrl();
+
+function getApiUrl() {
+  const isProduction = process.env.NODE_ENV === "production";
+
+  if (isProduction && process.env.NEXT_PUBLIC_VERCEL_URL) {
+    return process.env.NEXT_PUBLIC_VERCEL_URL;
+  }
+
+  return process.env.NEXT_PUBLIC_API_URL;
+}
--- src/pages/api/tags/[[...tags]].ts
+++ src/pages/api/tags/[[...tags]].ts
@@ -1,8 +1,11 @@
 import { ApiController } from "@server/controllers/api-controller";
+import mongoDbMiddleware from "@server/middlewares/mongodb";
 import { TagRepository } from "@server/repositories/tag.repository";
 import { ITag } from "@shared/models/tag.model";
-import { withController } from "next-controllers";
+import morgan from "morgan";
+import { UseMiddleware, withController } from "next-controllers";
 
+@UseMiddleware(morgan("dev"), mongoDbMiddleware())
 class TagApiController extends ApiController<ITag> {
   constructor() {
     super(new TagRepository(), {
