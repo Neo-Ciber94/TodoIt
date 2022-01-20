c0a6d9c9eb91c73cbcbd9dfa708b00357965f075
Freddy L <neo-ciber_94@hotmail.com>
2022-01-19 21:11:49 -0400

    Updated env for API_URLs

--- src/client/constants/index.ts
+++ src/client/constants/index.ts
@@ -1,4 +1,7 @@
 /**
  * The api URL.
  */
-export const API_URL = process.env.PUBLIC_NEXT_API_URL;
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? process.env.NEXT_PUBLIC_VERCEL_URL
+    : process.env.NEXT_PUBLIC_API_URL;
