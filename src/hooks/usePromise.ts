ecd1c6c86e5b69bbcabcf20f8c6a973ace56b0a7
Freddy L <neo-ciber_94@hotmail.com>
2022-01-22 03:25:28 -0400

    Added custom hook for usePromise

---
+++ src/hooks/usePromise.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+export interface UsePromiseOptions<T> {
+  value?: T;
+  isLoading: boolean;
+  error: Error | null;
+}
+
+export function usePromise<T>(promise: Promise<T>) {
+  const [value, setValue] = useState<T | undefined>(undefined);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setError(null);
+    
+    promise
+      .then((value) => setValue(value))
+      .catch((error) => setError(error))
+      .finally(() => setIsLoading(false));
+  }, [promise]);
+
+  return {
+    value,
+    isLoading,
+    error,
+  };
+}
