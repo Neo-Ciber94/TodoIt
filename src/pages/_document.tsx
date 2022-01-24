461b0ae9c9848fec6fe7c27d2523e3aa1417c4b9
Freddy L <neo-ciber_94@hotmail.com>
2022-01-24 18:15:32 -0400

    Added configuration for PWA

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,88 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head></Head>
+        <Head>
+          <meta name="application-name" content="TodoIt" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-title" content="TodoIt" />
+          <meta name="description" content="Best TodoIt in the world" />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta
+            name="msapplication-config"
+            content="/icons/browserconfig.xml"
+          />
+          <meta name="msapplication-TileColor" content="#2B5797" />
+          <meta name="msapplication-tap-highlight" content="no" />
+          <meta name="theme-color" content="#000000" />
+
+          <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="/icons/touch-icon-ipad.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/icons/touch-icon-iphone-retina.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="167x167"
+            href="/icons/touch-icon-ipad-retina.png"
+          />
+
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/icons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/icons/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/manifest.json" />
+          <link
+            rel="mask-icon"
+            href="/icons/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+          />
+
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:url" content="https://next-js-todo-app-mu.vercel.app/" />
+          <meta name="twitter:title" content="TodoIt" />
+          <meta
+            name="twitter:description"
+            content="An app for save the things you need todo"
+          />
+          <meta
+            name="twitter:image"
+            content="/icon-512x512.png"
+          />
+          <meta name="twitter:creator" content="NeoCiber" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="TodoIt" />
+          <meta property="og:description" content="An app for save the things you need todo" />
+          <meta property="og:site_name" content="TodoIt" />
+          <meta property="og:url" content="https://next-js-todo-app-mu.vercel.app/" />
+          <meta
+            property="og:image"
+            content="/icon-512x512.png"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
