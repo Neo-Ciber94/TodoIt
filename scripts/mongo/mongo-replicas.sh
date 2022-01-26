31101aa1f8c5ca2b5f52d67b409ecb0e3811a7a7
Freddy L <neo-ciber_94@hotmail.com>
2022-01-26 02:29:14 -0400

    Updated docker-compose

--- scripts/mongo/mongo-replicas.sh
+++ scripts/mongo/mongo-replicas.sh
@@ -7,15 +7,15 @@ var config = {
     "members": [
         {
             "_id": 0,
-            "host": "mongo1:27017"
+            "host": "mongo1:27018"
         },
         {
             "_id": 1,
-            "host": "mongo2:27017"
+            "host": "mongo2:27019"
         },
         {
             "_id": 2,
-            "host": "mongo3:27017"
+            "host": "mongo3:27020"
         }
     ]
 };
