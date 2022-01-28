e02b13018837f96581ff36e0e415ff088a202a7b
Freddy L <neo-ciber_94@hotmail.com>
2022-01-28 18:34:56 -0400

    Updated auth route

--- src/pages/api/auth/[[...auth0]].ts
+++ src/pages/api/auth/[[...auth0]].ts
@@ -1,17 +1,15 @@
-import {
-  AfterCallback,
-  getSession,
-  handleAuth,
-  handleCallback,
-  handleLogin,
-} from "@auth0/nextjs-auth0";
+import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
 
 export default handleAuth({
   async login(req, res) {
     try {
-      await handleLogin(req, res);
+      await handleLogin(req, res, {
+        authorizationParams: {
+          prompt: "select_account",
+        },
+      });
     } catch (error: any) {
       res.status(error.status || 500).end(error.message);
     }
-  }
+  },
 });
